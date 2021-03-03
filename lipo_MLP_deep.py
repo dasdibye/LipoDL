612d484086fba4618b07e9531699c23737ba08c5
dasdibye <58502905+dasdibye@users.noreply.github.com>
2021-03-03 10:56:32 +0530

    Update lipo_MLP_deep.py

--- lipo_MLP_deep.py
+++ lipo_MLP_deep.py
@@ -50,7 +50,7 @@ def evaluation(model, X_test, y_test):
     plt.figure(figsize=(15, 10))
     fig, ax = plt.subplots()
 
-    ax.scatter(prediction[:400], y_test[:400], c='r', linewidth=1.0)
+    ax.scatter(y_test[:400], prediction[:400], c='r', linewidth=1.0)
     plt.legend()
     ax.set_xlabel('Real logP')
     ax.set_ylabel('Predicted logP')
